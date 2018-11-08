b1ad674606b7c3baa86a7662b2e497df7b6f060a
CoconutBottle <chinming95@sohu.com>
2018-11-08 20:46:10 +0800

    新增Item

---
+++ items/AmapItem.py
@@ -0,0 +1,12 @@
+from BaseItem import iimediaItem
+
+class AmapItem(iimediaItem):
+    delayFat = iimediaItem.Field()
+    delayLeg = iimediaItem.Field()
+    cName = iimediaItem.Field()
+    distName  = iimediaItem.Field()
+    wRatio   = iimediaItem.Field()
+    avgSpd = iimediaItem.Field()
+    freeSpd = iimediaItem.Field()
+    areaCode = iimediaItem.Field()
+    pickTime = iimediaItem.Field()
