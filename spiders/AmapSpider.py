f8a773bb1aa686fe515a9a28c2686d1f3c7acdde
CoconutBottle <chinming95@sohu.com>
2018-11-08 17:20:05 +0800

    test

--- spiders/AmapSpider.py
+++ spiders/AmapSpider.py
@@ -1,1 +1,17 @@
-test
+#-*- coding: UTF-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import os
+currentUrl = os.path.dirname(__file__)
+parentUrl = os.path.abspath(os.path.join(currentUrl, os.pardir))
+sys.path.append(parentUrl)
+
+from BaseSpider import iimediaBase
+
+class Amap(iimediaBase):
+
+    def __init__(self):
+        start_urls = [
+            "http: // report.amap.com / ajax / getCityInfo.do?"
+        ]
